6961da00bb0ec5d8f8f8f249bd6b68c5071341d6
dragonetail <dragonetail@gmail.com>
2018-10-26 16:33:53 +0800

    added

---
+++ PhotoSaver/PhotoSaver/gallery/HeaderCell.swift
@@ -0,0 +1,9 @@
+//
+//  HeaderCell.swift
+//  PhotoSaver
+//
+//  Created by 孙玉新 on 2018/10/25.
+//  Copyright © 2018年 dragonetail. All rights reserved.
+//
+
+import Foundation
---
+++ PhotoSaver/PhotoSaver/gallery/sources/ImageSection.swift
@@ -0,0 +1,9 @@
+//
+//  ImageSection.swift
+//  PhotoSaver
+//
+//  Created by 孙玉新 on 2018/10/26.
+//  Copyright © 2018年 dragonetail. All rights reserved.
+//
+
+import Foundation
